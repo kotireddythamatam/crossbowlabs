8e2100c63f51d7429ee66a734e75d7d8a78f1f73
koti <kotireddythamatam@gmail.com>
2019-11-25 19:39:17 +0530

    ten

--- studentform/views.py
+++ studentform/views.py
@@ -3,10 +3,8 @@ from . forms import Student_login_form, Student_signup_form
 from . models import Student_model
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
-#This is student form for crossbowlabs.com task
 from django.http import HttpResponse, HttpResponseRedirect
-#This is from branch and i have made some changes to the branch koti now you can go with the master.
-#fifth command
+
 def Student_signup_view(request):
     if request.method == "GET":
         form = Student_signup_form()
@@ -30,7 +28,6 @@ def Student_login_view(request):
             else:
                 return HttpResponseRedirect('./login')
 
-#This is for displaying for all records.
 def display(request):
     obj = Student_model.objects.all()
     return render(request,'display.html',{'obj':obj})
@@ -55,5 +52,3 @@ def delete(request,id):
     delete_obj = Student_model.objects.get(id=id)
     delete_obj.delete()
     return HttpResponseRedirect('./display')
-
-#This is ending of views for task of crossbowlabs.com
